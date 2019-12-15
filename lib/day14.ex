a4e4f18dabf9066f52915cd5027ba9e7e5c6d1cc
Morten Bendiksen <morten.bendiksen@gmail.com>
2019-12-15 08:15:19 +0100

    fix Day14 a bit

--- lib/day14.ex
+++ lib/day14.ex
@@ -6,45 +6,36 @@ defmodule Day14 do
     [pname, pnum]
   end
 
-  def produce(prod, n, reactions, leftover) do
-    {outnum, reqs = [[frn, frin]|_]} = reactions[prod]
-    left = Map.get(leftover, prod, 0)
-    actualn = n - min(left, n)
-    left = left - min(left, n)
-    leftover = Map.put(leftover, prod, left)
-    extraprod = ((div(actualn, outnum) + min(1, rem(actualn, outnum))) * outnum) - actualn
-    leftover = Map.put(leftover, prod, extraprod + left)
-
-    if frn == "ORE" do
-      ore_need = frin * (div(actualn,outnum) + min(1, rem(actualn, outnum)))
-      {ore_need, leftover}
-    else
-      reqs |> Enum.reduce({0, leftover}, fn [rn, rin], {oneed, left} ->
-        in_need = rin * (div(actualn, outnum) + min(1, rem(actualn, outnum)))
-        {ore_need, leftover} = produce(rn, in_need, reactions, left)
-        {oneed + ore_need, leftover}
-      end)
-    end
+  def read_input(file) do
+    File.stream!(file)
+    |> Stream.map(&String.trim/1)
+    |> Enum.reduce(%{}, fn s, acc ->
+      [req, p] = String.split(s, " => ")
+      [pname, pnum] = parse_chem(p)
+      requirements = req |> String.split(", ") |> Enum.map(&parse_chem/1)
+      Map.put(acc, pname, {pnum, requirements})
+    end)
   end
 
-  def part1 do
-    react =
-      "input14.txt"
-      |> File.stream!()
-      |> Stream.map(&String.trim/1)
-      |> Enum.reduce(%{}, fn s, acc ->
-        [req, p] = String.split(s, " => ")
-        [pname, pnum] = parse_chem(p)
-        requirements = req |> String.split(", ") |> Enum.map(&parse_chem/1)
-        Map.put(acc, pname, {pnum, requirements})
-      end) #|> IO.inspect
+  def produce(prod, n, reactions, surplus) do
+    {outnum, reqs} = reactions[prod]
+    left_in_surplus = Map.get(surplus, prod, 0)
+    actualn = n - min(left_in_surplus, n)
+    left_in_surplus = left_in_surplus - min(left_in_surplus, n)
+    overprod = ((div(actualn, outnum) + min(1, rem(actualn, outnum))) * outnum) - actualn
+    surplus = Map.put(surplus, prod, overprod + left_in_surplus)
 
-    {needs, _left} = produce("FUEL", 1, react, %{})
-    IO.puts needs
+    Enum.reduce(reqs, {0, surplus}, fn [rn, rin], {total_ore, surplus} ->
+      in_need = rin * (div(actualn, outnum) + min(1, rem(actualn, outnum)))
+      {ore_need, surplus} = case rn do
+        "ORE" -> {in_need, surplus}
+        _ -> produce(rn, in_need, reactions, surplus)
+      end
+      {total_ore + ore_need, surplus}
+    end)
   end
 
-  def search(guess, low, high, react) do
-    ore = 1000000000000
+  def search(react, guess, low, high, ore) do
     {needs, _left} = produce("FUEL", guess, react, %{})
     cond do
       high - low <= 1 ->
@@ -52,26 +43,27 @@ defmodule Day14 do
       needs < ore ->
         low = guess
         guess = guess + div((high-low), 2)
-        search(guess, low, high, react)
+        search(react, guess, low, high, ore)
       needs > ore ->
         high = guess
         guess = guess - div((high-low), 2)
-        search(guess, low, high, react)
+        search(react, guess, low, high, ore)
       end
   end
 
-  def part2 do
-    react =
-      "input14.txt"
-      |> File.stream!()
-      |> Stream.map(&String.trim/1)
-      |> Enum.reduce(%{}, fn s, acc ->
-        [req, p] = String.split(s, " => ")
-        [pname, pnum] = parse_chem(p)
-        requirements = req |> String.split(", ") |> Enum.map(&parse_chem/1)
-        Map.put(acc, pname, {pnum, requirements})
-      end) #|> IO.inspect
+  def calculate_fuel_from_ore(react, ore) do
+     search(react, 1, 0, ore, ore)
+  end
 
-    IO.puts search(0, 0, 10000000, react)
+  def part1 do
+    react = read_input("input14.txt")
+    {needs, _left} = produce("FUEL", 1, react, %{})
+    IO.puts needs
+  end
+
+  def part2 do
+    read_input("input14.txt")
+    |> calculate_fuel_from_ore(1000000000000)
+    |> IO.puts()
   end
 end
